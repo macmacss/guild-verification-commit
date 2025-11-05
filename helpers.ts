994ebb465850ae1dc52bde8b4a17bf7618102a3e
nord <123259238+macmacss@users.noreply.github.com>
2025-11-06 02:48:31 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:26:29.444Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T23:48:30.361Z
+Commit: Fix CSS styling bug on mobile devices
