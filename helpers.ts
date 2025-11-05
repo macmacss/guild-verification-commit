f98b58c7564b1b8a147d8c92dd552fb51c58b512
nord <123259238+macmacss@users.noreply.github.com>
2025-11-06 02:48:44 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:48:41.305Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-05T23:48:43.541Z
+Commit: Add multi-language support
