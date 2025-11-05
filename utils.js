6985e00548f6d3bc02d3cb8aa92a4c5f612b87cc
nord <123259238+macmacss@users.noreply.github.com>
2025-11-06 02:51:26 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:51:02.135Z
-Commit: Fix database connection timeout
+Timestamp: 2025-11-05T23:51:24.806Z
+Commit: Resolve null pointer exception in user service
