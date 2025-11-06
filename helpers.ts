18b7aa857cf69b53b6148c2ac3c0d3203898fedc
nord <123259238+macmacss@users.noreply.github.com>
2025-11-06 03:07:09 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:06:38.973Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T00:07:08.332Z
+Commit: Fix memory leak in event handlers
