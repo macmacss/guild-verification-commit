acb8c1616070ce38c1967e6894b3d82d9a1b093d
nord <123259238+macmacss@users.noreply.github.com>
2025-11-06 03:00:15 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:56:06.425Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T00:00:14.617Z
+Commit: Add dark mode support
